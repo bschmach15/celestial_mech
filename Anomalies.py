1d2e136ffec28922576cec87f4ce67bd5c4961da
Benjamin Schmachtenberger <bschmach15@gmail.com>
2016-09-20 18:14:53 -0400

    commiting the Anomalies file

--- Anomalies.py
+++ Anomalies.py
@@ -3,7 +3,8 @@ import scipy.constants as spc
 
 class Anomalies:
 
-    def __init__(self, planet, eccentricity, semimajor_axis, true_anomaly = None, mean_anomaly= None, eccentric_anomaly = None, time_past_periapse = None):
+    def __init__(self, eccentricity, semimajor_axis, planet = None, true_anomaly = None, mean_anomaly= None,
+                 eccentric_anomaly = None, time_past_periapse = None):
         self.planet = planet
         self.eccentricity = eccentricity
         self.semimajor_axis = semimajor_axis
@@ -11,7 +12,9 @@ class Anomalies:
         self.mean_anomaly = mean_anomaly
         self.eccentric_anomaly = eccentric_anomaly
         self.time_past_periapse = time_past_periapse
-        self.mean_motion = math.sqrt(spc.G * planet._radius/(self.semimajor_axis**3))
+        if self.planet is not None:
+            self.mean_motion = math.sqrt(spc.G * planet._radius/(self.semimajor_axis**3))
+        self.main()
 
     def sec_to_min(self):
         self.time_past_periapse = self.time_past_periapse / 60
@@ -24,7 +27,7 @@ class Anomalies:
 
     def eccentric_to_true(self):
         ecc_anomaly_rads = math.radians(self.eccentric_anomaly)
-        self.true_anomaly = math.degrees(2 * math.atan(math.sqrt((1. + self.eccentricity) / (1. - self.eccentricity)) * math.tan(self.eccentric_anomaly/ 2.)))
+        self.true_anomaly = math.degrees(2 * math.atan(math.sqrt((1. + self.eccentricity) / (1. - self.eccentricity)) * math.tan(ecc_anomaly_rads/ 2.)))
         if self.true_anomaly < 0:
             self.true_anomaly += 360
 
@@ -33,14 +36,15 @@ class Anomalies:
 
     def mean_to_eccentric(self, tolerance = 1 * 10**(-8)):
         mean_anomaly_rads = math.radians(self.mean_anomaly)
-        if (mean_anomaly_rads> spc.pi and mean_anomaly_rads< 0) or mean_anomaly_rads> spc.pi:
-            E_current = mean_anomaly_rads- self.eccentricity
+        if spc.pi < mean_anomaly_rads < 0 or mean_anomaly_rads > spc.pi:
+            E_current = mean_anomaly_rads - self.eccentricity
         else:
             E_current = mean_anomaly_rads + self.eccentricity
         difference = 1
         while abs(difference) > tolerance:
             E_previous = E_current
-            E_current = E_previous + (mean_anomaly_rads- E_previous + self.eccentricity * math.sin(E_previous))/(1 - self.eccentricity * math.cos(E_previous))
+            E_current = E_previous + (mean_anomaly_rads - E_previous + self.eccentricity * math.sin(E_previous))/(1 -
+                                                                                                                  self.eccentricity * math.cos(E_previous))
             difference = abs(E_current - E_previous)
         else:
             self.eccentric_anomaly = math.degrees(E_current)
@@ -51,6 +55,9 @@ class Anomalies:
 
     def tpp_to_mean(self):
         self.mean_anomaly = self.mean_motion * self.time_past_periapse * 60
+
+    def eccentric_to_altitude(self):
+        pass
 
     def main(self):
         if self.true_anomaly is not None:
